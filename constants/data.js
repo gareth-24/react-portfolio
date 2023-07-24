5bdd795a1152d45f0c0d5a2a1e9d20aaf7ab0102
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-24 15:47:57 -0700

    add project files

---
+++ constants/data.js
@@ -0,0 +1,113 @@
+const userData = {
+  githubUsername: "gareth-24",
+  name: "Gareth Grindeland",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/prof.png",
+  email: "gt.grindeland@gmail.com",
+  phone: "2067991936",
+  address: "Seattle, WA, USA",
+  projects: [
+    // {
+    //   title: "PlaceholderTech",
+    //   link: "https://placeholdertech.in",
+    //   imgUrl: "/placeholdertech.png",
+    // },
+    {
+      title: "Work in progress: Epicodus Capstone",
+      link: "https://github.com/gareth-24/capstone",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "Next.js Blog",
+      link: "https://nextjs-blog-gareth-24.vercel.app/",
+      imgUrl: "/nextjs-blog.png",
+    },
+    {
+      title: "The One API",
+      link: "https://github.com/gareth-24/The_One_API.Solution",
+      imgUrl: "/theoneapi.png",
+    },
+    {
+      title: "Recipe Box",
+      link: "https://github.com/gareth-24/RecipeBox.Solution",
+      imgUrl: "/recipe-box.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://gareth-24.github.io/portfolio/",
+      imgUrl: "/portfolio.png",
+    },
+    // {
+    //   title: "Feedmeback",
+    //   link: "https://feedmeback.vercel.app",
+    //   imgUrl: "/feedmeback.png",
+    // },
+    // {
+    //   title: "Devmedium",
+    //   link: "https://devmedium.vercel.app",
+    //   imgUrl: "/devmedium.png",
+    // },
+  ],
+  about: {
+    title:
+      "I'm an aspiring full-stack developer with a passion for technical problem solving, collaborating on open-source projects, and bringing communities together",
+    description: [
+      `I am a former Civil Design Engineer transitioning to a career in web development and programming. I am currently enrolled in the full time, full stack, C# and React Epicodus coding bootcamp.`,
+      `At Epicodus, I am learning to build web applications using JavaScript, React, C#/.NET, HTML, CSS, and more. Most importantly, I am learning how to think like a programmer and write clean and scalable code. My previous experience as a civil engineer and student at the University of Washington has helped me tackle complex coding problems by breaking them down to simple steps of logic. My design and permitting experience has also helped me when navigating external resources to find pertinent and accurate information for projects.`,
+      `Outside of work and the classroom, I am an avid pacific northwest sports fan as well as a video game enthusiast. When I am not coding, I enjoy traveling, barhopping with friends, lifting weights, attending UW football and Mariners games, and hopping on Discord to relax and play co-op games on weekends.`
+    ],
+    currentProject: "Epicodus Capstone",
+    currentProjectUrl: "https://github.com/gareth-24/capstone",
+  },
+  experience: [
+
+    {
+      title: "Full-stack Coding Bootcamp",
+      company: "Epicodus",
+      year: "2023",
+      // companyLink: "https://www.epicodus.com/",
+      desc: "Full-time C# and React coding bootcamp focused on open-source and web development. Built 25+ projects independently and in collaboration with other students. Experience with GIT, JavaScript, Typescript, HTML, CSS, C#/.NET, React, MySQL, NoSQL, Firestore, Next.js, and more.",
+    },
+    {
+      title: "Project Engineer",
+      company: "AHBL, Tacoma, WA",
+      year: "2020",
+      // companyLink: "https://intel.com",
+      desc: "Assisted project manager and principal engineer in the design and permitting of large and small scale industrial, commercial, and residential projects. Using Civil 3D, drafted construction plans for all project phases from preliminary design and site development to final design and as-builts. ",
+    },
+    {
+      title: "University of Washington, Seattle",
+      company: "Bachelor of Science in Civil Engineering",
+      year: "2019",
+      companyLink: "https://upes.co.in",
+      desc: "Graduated with an upper division coursework GPA of 3.60. As a member of the Sigma Phi Epsilon fraternity, I was a brother mentor for fellow first-year engineering students and also a Dawg Daze Leader for UW's First Year Programs.",
+    },
+    {
+      title: "Server Assistant",
+      company: "Heartwood Provisions",
+      year: "2017",
+      // companyLink: "https://upes.co.in",
+      desc: "While pursuing my degree, I worked at Heartwood in downtown Seattle, assisting servers and barstaff with running food, table maintenance, and greeting guests.",
+    },
+    {
+      title: "High School Graduation",
+      company: "Bainbridge High School",
+      year: "2015",
+      // companyLink: "https://upes.co.in",
+      desc: "I grew up in Bainbridge Island, WA. While at BHS, I played football, baseball, chess, and was even on the cheer team!",
+    },
+  ],
+  resumeUrl:
+    "https://docs.google.com/document/d/1hbdTctJ0dfbUTUsnioZma_1ogK0AFw2Q5_4Eea51ehE/edit?usp=sharing",
+  socialLinks: {
+    // instagram: "https://instagram.com/maninthere",
+    twitter: "https://twitter.com/GGrinz",
+    linkedin: "https://linkedin.com/in/gareth-grindeland",
+    github: "https://github.com/gareth-24",
+    indeed: "https://profile.indeed.com/p/garethg-dhszxkj"
+    // facebook: "https://facebook.com/",
+    // peerlist: "https://peerlist.io/manuarora",
+  },
+};
+
+export default userData;
